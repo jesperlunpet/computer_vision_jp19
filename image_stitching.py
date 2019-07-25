dd57296f20d5af33641ee93ce16412a37961fa2b
jesperlunpet <s140456@student.dtu.dk>
2019-07-25 22:56:08 +0900

    Updated

--- image_stitching.py
+++ image_stitching.py
@@ -8,21 +8,6 @@ MIN_MATCH_COUNT = 10
 
 # Buzz words: Domain specific, 
 
-def trim(frame):
-    #crop top
-    if not np.sum(frame[0]):
-        return trim(frame[1:])
-    #crop top
-    if not np.sum(frame[-1]):
-        return trim(frame[:-2])
-    #crop top
-    if not np.sum(frame[:,0]):
-        return trim(frame[:,1:])
-    #crop top
-    if not np.sum(frame[:,-1]):
-        return trim(frame[:,:-2])
-    return frame
-
 def featureDetection(img1, img2, detector):
 	# FDetect the keypoints with SIFT Detector, compute the descriptors
 	if detector.lower() == "sift": 
@@ -30,17 +15,21 @@ def featureDetection(img1, img2, detector):
 	elif detector.lower() == "surf":
 		fdet = cv2.xfeatures2d.SURF_create()
 	elif detector.lower() == "orb":
-		fdet = cv2.ORB_create(nfeatures=1500)
-		kp0 = fdet.detect(img1,None)
-		kp1 = fdet.detect(img2,None)
+		fdet = cv2.ORB_create(nfeatures=500)
+		kp0 = fdet.detect(img1, None)
+		kp1 = fdet.detect(img2, None)
 		kp0, des0 = fdet.compute(img1, kp0)
 		kp1, des1 = fdet.compute(img2, kp1)
+		# cv2.imshow('original_image_left_keypoints'+str(detector)+".jpg", cv2.drawKeypoints(img1, kp0, None, color=(0,0,255)))
+		# cv2.imwrite( "dist/"+str(detector)+".jpg", cv2.drawKeypoints(img1, kp0, None, color=(0,0,255)) );
 		return kp0, des0, kp1, des1
 	else:
 		raise Exception("{} is not a valid input for the detector".format(detector))
 
 	kp0, des0 = fdet.detectAndCompute(img1, None)
 	kp1, des1 = fdet.detectAndCompute(img2, None)
+	# cv2.imwrite( "dist/"+str(detector)+".jpg", cv2.drawKeypoints(img1, kp0, None, color=(0,0,255)) );
+	# cv2.imshow('original_image_left_keypoints'+str(detector)+".jpg", cv2.drawKeypoints(img1, kp0, None, color=(0,0,255)))
 	return kp0, des0, kp1, des1
 
 def getMatches(des1,des2, matcher):
@@ -50,7 +39,7 @@ def getMatches(des1,des2, matcher):
 	good = []
 	# Filtering the matches
 	for m,n in matches:
-		if m.distance < 0.3*n.distance:
+		if m.distance < 0.4*n.distance:
 			good.append(m)
 	return good
 
@@ -68,41 +57,36 @@ def combine(img1_, img2_, matcher, detector):
 		dst = cv2.perspectiveTransform(pts, M)
 		img2 = cv2.polylines(img1,[np.int32(dst)],True,255,3, cv2.LINE_AA)
 		dst = cv2.warpPerspective(img1_,M,(img2_.shape[1] + img1_.shape[1], img2_.shape[0]))
+		# cv2.imwrite( "dist/warped.jpg", dst) 
+		# cv2.imshow("warpPerspective.jpg", dst)
 		img3 = cv2.drawMatches(img1_, kp1, img2_, kp2, matches, None, matchColor=(255,0,0), singlePointColor=None, flags=2)
-		cv2.imshow("original_image_drawMatches"+str(matcher)+".jpg", img3)
+		# cv2.imwrite( "dist/"+str(matcher)+".jpg", img3) 
+		# cv2.imshow("original_image_drawMatches"+str(matcher)+".jpg", img3)
 		dst[0:img2_.shape[0],0:img2_.shape[1]] = img2_
 		# cv2.imshow("original_image_stitched.jpg", dst)
 		return dst
-		# cv2.imshow("original_image_overlapping.jpg", img2)
 	else:
-		raise Exception("Not enought matches are found - %d/%d", (len(matches)/MIN_MATCH_COUNT))
+		raise Exception("Not enought sorted matches are found - {}".format(MIN_MATCH_COUNT))
 
-img0_ = cv2.imread("images/IMG_1786-2.jpg")
-img1_ = cv2.imread("images/IMG_1787-2.jpg")
-img2_ = cv2.imread("images/IMG_1788-2.jpg")
+img0_ = cv2.imread("images/IMG_3118.jpg")
+img1_ = cv2.imread("images/IMG_3119.jpg")
+img2_ = cv2.imread("images/IMG_3120.jpg")
 
-images = []
- 
-imagePaths = sorted(list(paths.list_images("images/")))
+# combine(img0_, img1_, cv2.DESCRIPTOR_MATCHER_FLANNBASED, "sift")
+# combine(img0_, img1_, cv2.DESCRIPTOR_MATCHER_BRUTEFORCE, "sift")
+img3_ = combine(img0_, img1_, cv2.DESCRIPTOR_MATCHER_BRUTEFORCE, "sift")
+# img4_ = combine(img1_, img2_, cv2.DESCRIPTOR_MATCHER_BRUTEFORCE, "sift")
+# img5_ = combine(img3_, img4_, cv2.DESCRIPTOR_MATCHER_BRUTEFORCE, "sift")
+# img6_ = combine(img0_, img4_, cv2.DESCRIPTOR_MATCHER_BRUTEFORCE, "sift")
+# img7_ = combine(img3_, img2_, cv2.DESCRIPTOR_MATCHER_BRUTEFORCE, "sift")
 
-for imagePath in imagePaths:
-	image = cv2.imread(imagePath)
-	images.append(image)
-
-print(imagePaths)
-
-print(images)
-
-stitcher = cv2.Stitcher_create()
-(status, stitched) = stitcher.stitch(images)
-
-print(status)
-print(stitched)
-
-img3_ = combine(img0_, img1_, cv2.DESCRIPTOR_MATCHER_FLANNBASED, "surf")
-img4_ = combine(img0_, img1_, cv2.DESCRIPTOR_MATCHER_BRUTEFORCE, "sift")
-
-# cv2.imshow("finalFLANN.jpg", trim(img3_))
-# cv2.imshow("finalBRUTE.jpg", trim(img4_))
+cv2.imshow("in1.jpg", img3_)
+# cv2.imwrite( "dist/result1.jpg", img3_) 
+# cv2.imshow("final2.jpg", img5_)
+# cv2.imwrite( "dist/result2.jpg", img5_) 
+# cv2.imshow("final2.jpg", img6_)
+# cv2.imwrite( "dist/result3.jpg", img6_) 
+# cv2.imshow("in.jpg", img7_)
+# cv2.imwrite( "dist/result4.jpg", img7_) 
 cv2.waitKey(0)
 #cv2.imsave("original_image_stitched_crop.jpg", trim(dst))
